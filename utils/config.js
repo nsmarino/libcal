9444271e8f2b39dcffbb1e4f145e89f6c69e7cae
Nicholas Marino <nsmarino0@gmail.com>
2020-10-04 12:37:24 -0400

    added logger

--- utils/config.js
+++ utils/config.js
@@ -3,6 +3,9 @@ let api = 'https://reedevents.vercel.app/api'
 if (process.env.NODE_ENV === 'development') {  
     api = 'http://localhost:3000/api'
   }
+if (process.env.NODE_ENV === 'test') {
+  api = 'http://localhost:3000/api'
+}
 
 module.exports = {
     api
